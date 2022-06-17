b2e77d354f4226a90f358ce17dd0dd2774130d17
Rkanraeden <lecortezc@gmail.com>
2022-06-17 04:39:42 -0400

    feat devisefeat: Config controller devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :posts
   resources :jams
-  devise_for :users
+  devise_for :users, controllers: {
+              registrations: 'users/registrations'
+            }
   get 'home/index'
   root to: "home#index"
 end
